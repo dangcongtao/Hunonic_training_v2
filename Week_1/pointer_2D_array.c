c60d796427fdd8d0bb31cb4f133ebf71d394b1d5
dangcongtao <dangcongtao1999tn@gmail.com>
2022-05-04 06:39:14 -0400

    Pointer with 2D array: WARNNING: 1D+2D Array is not deallowcate

---
+++ Week_1/pointer_2D_array.c
@@ -0,0 +1,69 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE1 3
+#define SIZE2 2
+
+
+
+
+void input(int ***a) {
+    *a = (int**)malloc(SIZE1*sizeof(int*));
+    for ( int i = 0; i < SIZE1; i ++) {
+        (*a)[i] = (int*)malloc(SIZE2*sizeof(int));
+    }
+
+    int temp = 0;
+    printf("here1\n");
+    
+    for (int i = 0 ; i < SIZE1; i ++) {
+        for (int j = 0 ; j < SIZE2 ; j++) {
+            scanf("%d", &temp);
+            (*a)[i][j] = temp;
+        }
+    } 
+   
+}
+
+/* doing here */
+void arrange(int ***a) {
+    int swap = 0;
+    for (int i = 0 ; i < SIZE1; i ++) {
+        for (int j = 0 ; j < SIZE2; j++) {
+            for (int k = j; k < SIZE2; k++) {
+                if ((*a)[i][j] > (*a)[i][k]) {
+                    swap = (*a)[i][k];
+                    (*a)[i][k] = (*a)[i][j];
+                    (*a)[i][j] = swap;
+                }
+            }
+        }
+    }
+}
+
+void output(int **a) {
+    for ( int i = 0; i < SIZE1; i++) {
+        for ( int j = 0; j < SIZE2; j++) {
+            printf("%d ", a[i][j]);
+        }printf("\n");
+    }
+    printf("\n");
+}
+
+
+
+int main() {
+
+    int **a;
+
+    input(&a);
+    output(a);
+
+    arrange(&a);
+    output(a);
+    
+    /* deallowcate */
+
+    return 0;
+}
